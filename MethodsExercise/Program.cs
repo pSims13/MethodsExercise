73b2e6bcda510f406ca5a5b85b759dbb38b4d63b
Parker Sims <parker4al@gmail.com>
2025-04-03 20:03:47 -0500

    Completed C# story program exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,9 +2,57 @@
 {
     public class Program
     {
+        //Method to collect persons favourite arcade game
+        public static string GetPersonsFavouriteArcadeGame()
+        {
+            Console.WriteLine("What is your favourite arcade game?");
+
+            string arcadeGame = Console.ReadLine();
+            return arcadeGame;
+
+        }
+        
+        //Method to collect how much money the person has
+        public static decimal GetNumberOfDollarsFromPerson()
+        {
+            Console.WriteLine("How many dollars do you have?");
+            
+            decimal dollars = decimal.Parse(Console.ReadLine());
+            return dollars;
+        }
+            
+        //Method to collect what the persons favourite color is
+        public static string GetPersonsFavouriteColor()
+        {
+            Console.WriteLine("What is your favourite color?");
+
+            string color = (Console.ReadLine());
+            return color;
+        }
+        
+        //Method to collect the persons name
+        public static string GetPersonNameFromUser()
+        {
+            Console.WriteLine("Please give me your name:");
+
+            string name = (Console.ReadLine());
+            return name;
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //Assigning values to variables by calling their respective methods
+            string name = GetPersonNameFromUser();
+            string color = GetPersonsFavouriteColor();
+            decimal dollars = GetNumberOfDollarsFromPerson();
+            string arcadeGame = GetPersonsFavouriteArcadeGame();
+            
+            //Used string interpolation to creat a short story from the info collected from user
+            Console.WriteLine($"Hello my name is {name}. Growing up and still today my favourite color has always been {color}.");
+            Console.WriteLine($"I've been working really hard and managed to save up ${dollars}. That should be enough to play several games of my favourite arcade game {arcadeGame}!");
+
+
         }
     }
 }
